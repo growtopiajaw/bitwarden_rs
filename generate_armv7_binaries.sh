5dbced667f6ecdf21a01737dea92d0a568667b3f
Growtopia Jaw <growtopiajaw@gmail.com>
2021-05-10 23:55:09 +0800

    Reflect bitwarden_rs name changes

--- generate_armv7_binaries.sh
+++ generate_armv7_binaries.sh
@@ -1,13 +1,13 @@
 #! /bin/bash
-docker pull --platform linux/arm/v7 bitwardenrs/server:alpine
-docker create --name bwrs bitwardenrs/server:alpine
-docker cp bwrs:/bitwarden_rs .
-docker cp bwrs:/web-vault .
-docker rm bwrs
+docker pull --platform linux/arm/v7 vaultwarden/server:alpine
+docker create --name vwrs vaultwarden/server:alpine
+docker cp vwrs:/vaultwarden .
+docker cp vwrs:/web-vault .
+docker rm vwrs
 cat web-vault/version.json | sed 's/{"version":"//g' | sed 's/"}//g'
 sudo apt install p7zip* -y
-7z a bitwarden_rs-$(cat web-vault/version.json | sed 's/{"version":"//g' | sed 's/"}//g').zip bitwarden_rs web-vault
-rm -rf web-vault bitwarden_rs
+7z a bitwarden_rs-$(cat web-vault/version.json | sed 's/{"version":"//g' | sed 's/"}//g').zip vaultwarden web-vault
+rm -rf web-vault vaultwarden
 mv bitwarden* archive/
 git add .
 git commit -m "Update bitwarden_rs to version $(ls archive/bitwarden* | head -n 1 | sed 's/bitwarden_rs-//g' | sed 's/.zip//g')"
