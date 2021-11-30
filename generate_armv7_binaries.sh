0cada42d831a97bbd4f52443878493e6bff2e1c3
Growtopia Jaw <growtopiajaw@gmail.com>
2021-11-30 13:32:13 +0800

    Update 12

--- generate_armv7_binaries.sh
+++ generate_armv7_binaries.sh
@@ -4,11 +4,11 @@ docker create --name vwrs vaultwarden/server:alpine
 docker cp vwrs:/vaultwarden .
 docker cp vwrs:/web-vault .
 docker rm vwrs
-cat web-vault/version.json | sed 's/{"version":"//g' | sed 's/"}//g'
 sudo apt install p7zip* -y
+rm -rf archive/*
 7z a bitwarden_rs-$(cat web-vault/version.json | sed 's/{"version":"//g' | sed 's/"}//g').zip vaultwarden web-vault
 rm -rf web-vault vaultwarden
 mv bitwarden* archive/
 git add .
-git commit -m "Update bitwarden_rs to version $(ls archive/bitwarden* | head -n 1 | sed 's/bitwarden_rs-//g' | sed 's/.zip//g')"
+git commit -m "Update bitwarden_rs to version $(ls archive | head -n 1 | sed 's/bitwarden_rs-//g' | sed 's/.zip//g')"
 git push origin master
