b26bedd99ea913211ef8f6b254902b6e1b195546
Growtopia Jaw <growtopiajaw@gmail.com>
2023-04-21 10:49:06 +0800

    Update generate_armv7_binaries.sh

--- generate_armv7_binaries.sh
+++ generate_armv7_binaries.sh
@@ -1,14 +1,14 @@
 #! /bin/bash
 docker pull --platform linux/arm/v7 vaultwarden/server:alpine
-docker create --name vwrs vaultwarden/server:alpine
-docker cp vwrs:/vaultwarden .
-docker cp vwrs:/web-vault .
-docker rm vwrs
+docker create --name vw vaultwarden/server:alpine
+docker cp vw:/vaultwarden .
+docker cp vw:/web-vault .
+docker rm vw
 sudo apt install p7zip* -y
 rm -rf archive/*
-7z a bitwarden_rs-$(cat web-vault/version.json | sed 's/{"version":"//g' | sed 's/"}//g').zip vaultwarden web-vault
+7z a bitwarden_rs-"$(grep version web-vault/version.json | sed 's/{"version":"//g' | sed 's/"}//g')".zip vaultwarden web-vault
 rm -rf web-vault vaultwarden
 mv bitwarden* archive/
 git add .
-git commit -m "Update bitwarden_rs to version $(ls archive | head -n 1 | sed 's/bitwarden_rs-//g' | sed 's/.zip//g')"
+git commit -m "Update bitwarden_rs to version $(find archive -type f -printf '%P\n' | head -n 1 | sed 's/bitwarden_rs-//g' | sed 's/.zip//g')"
 git push origin master
